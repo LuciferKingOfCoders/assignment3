60110b0a54e88b71ef7a752af09f9f39434eaab5
SREENADTH KRISHNA <134028402+LuciferKingOfCoders@users.noreply.github.com>
2023-12-06 22:33:49 -0500

    addingcommentModel

---
+++ commentModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    rating: { type: Number, required: true },
+    images: [String],
+    text: { type: String, required: true },
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
